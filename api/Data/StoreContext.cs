efdac7305a5e549e9020173fe8428e3e3248f124
unknown <g.mohebi01@gmail.com>
2022-10-31 17:55:38 +0330

    add product class and EF core

---
+++ api/Data/StoreContext.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.DbContext
+
+namespace api.Data
+{
+    public class StoreContext : DbContext
+    {
+        
+    }
+}
