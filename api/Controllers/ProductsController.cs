80b85844f7a54db8288507e410bf1eade4c2ad01
Gandom <g.mohebi01@gmail.com>
2022-11-01 15:53:00 +0330

    add sqlite to project and migration

--- api/Controllers/ProductsController.cs
+++ api/Controllers/ProductsController.cs
@@ -1,22 +1,43 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using api.Data;
+using api.Entities;
 
 namespace api.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     public class ProductsController : ControllerBase
     {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
 
         [HttpGet]
-        public string GetProducts()
+        public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            return "this will be a list of products";
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
         }
 
         [HttpGet("{id}")]
-        public string GetProduct(int id)
+        public async Task<Product?> GetProduct(int id)
         {
-            return "sinlge product";
+            return await _context.Products.FindAsync(id);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product?>> GetProduct2(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct1(int id)
+        {
+            return Ok(await _context.Products.FindAsync(id));
         }
 
     }
